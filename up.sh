d6d94f3aae3a542c0195c263cf7c7b171c4cd752
Adi Wabisabi <awabisabi@confluent.io>
2023-11-27 12:57:05 -0700

    initial bits

---
+++ up.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+export CONFIG_FILE='client.properties'
+
+# Check if $CONFIG_FILE exists
+if [ -e "$CONFIG_FILE" ]; then
+    # Display existing configuration information
+    echo "An existing configuration ($CONFIG_FILE) has been found: $(grep '^bootstrap\.servers=' $CONFIG_FILE)"
+
+    # Ask the user if they want to use the existing configuration
+    read -p "Do you want to use this existing configuration? (y/n): " use_existing
+
+    if [ "$use_existing" == "y" ] || [ "$use_existing" == "Y" ]; then
+        echo "Using the existing configuration."
+    else
+        # Prompt the user to paste the new configuration
+        echo "Please paste the new configuration from Confluent Cloud and press Ctrl-D when complete:"
+        IFS='' # Set Internal Field Separator to empty to preserve leading and trailing whitespace
+        config_text=$(cat)
+
+        # Check if the user entered any text
+        if [ -z "$config_text" ]; then
+            echo "No input provided. Exiting."
+            exit 1
+        fi
+
+        # Write the new configuration to $CONFIG_FILE
+        echo -e "$config_text" > $CONFIG_FILE
+
+        echo "New configuration successfully written to $CONFIG_FILE."
+    fi
+else
+    # Prompt the user to paste the configuration
+    echo "Please paste the configuration from Confluent Cloud and press Ctrl-D when complete:"
+    IFS='' # Set Internal Field Separator to empty to preserve leading and trailing whitespace
+    config_text=$(cat)
+
+    # Check if the user entered any text
+    if [ -z "$config_text" ]; then
+        echo "No input provided. Exiting."
+        exit 1
+    fi
+
+    # Write the configuration to $CONFIG_FILE
+    echo -e "$config_text" > $CONFIG_FILE
+
+    echo "Configuration successfully written to $CONFIG_FILE."
+fi
+
+# Run the docker-compose up command with $CONFIG_FILE linked as a volume
+docker-compose up 
+
