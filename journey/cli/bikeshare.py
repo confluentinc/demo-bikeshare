6ac2adfdab061da86dde8b1d89b7e9cef36e5f5a
Adi Wabisabi <awabisabi@confluent.io>
2023-12-12 20:53:10 -0700

    remove nested subcommand as there's only one action really being used

--- journey/cli/bikeshare.py
+++ journey/cli/bikeshare.py
@@ -10,14 +10,11 @@ from rich.status import Status
 
 from journey.globals import GLOBALS
 from journey.data.source import gbfs
-from journey.data.kafka.producer import produce, multiple_producer_fanout
+from journey.data.kafka.producer import multiple_producer_fanout
 from journey.data.kafka.admin import create_topic, serializer_for_schema
 from journey.cli.textual.systems import SystemsTreeApp
 
 bikes_menu = Typer()
-produce_menu = Typer()
-
-bikes_menu.add_typer(produce_menu, name="produce")
 
 _system_id_from_label = lambda label: label.split('(')[-1].replace(')', '')
 
@@ -62,35 +59,11 @@ def systems():
     '''
     _systems_tree_controller_dialog()
     
-@produce_menu.command()
-def station_data(system_id:Annotated[str, Option(help='ID of system to use - use `journey bikeshare systems` to see a list')]=''):
-    '''
-    Load station data 
-    '''
     
-    if system_id == '':
-        system_id = _systems_tree_controller_dialog()
-    
-    valid = True
-    try:
-        stations = gbfs.system_stations_by_id(system_id)
-    except:
-        valid = False
-        
-    if not valid or stations is None or len(stations) == 0:
-        print(f'No stations found in system {system_id} - please try another system')
-        exit(1)
-        
-    print(f'{len(stations)} stations found in system {system_id}')
-    
-    topic = f'{system_id}.station.info'
-    create_topic(GLOBALS['cc_config'], topic)
-    produce(GLOBALS['cc_config'], topic, stations)
-    
-@produce_menu.command()
-def station_statuses(system_id:Annotated[str, Option(help='ID of system to use - use `journey bikeshare systems` to see a list')]='',
-                     produce_forever:Annotated[bool, Option(help='Produce data forever')]=False,
-                     fanout_size:Annotated[int, Option(help='Number of producers to fan out to - reduce number if you see errors - increase to improve performance')]=6):
+@bikes_menu.command()
+def produce(system_id:Annotated[str, Option(help='ID of system to use - use `journey bikeshare systems` to see a list')]='',
+            produce_forever:Annotated[bool, Option(help='Produce data forever')]=False,
+            fanout_size:Annotated[int, Option(help='Number of producers to fan out to - reduce number if you see errors - increase to improve performance')]=6):
     '''
     (Continuously) load station statuses
     '''
